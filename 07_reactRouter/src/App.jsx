4ff04f633caef93b6724a4264ec0ad250d08a162
vivekChandra7 <vivekchandra2462@gmail.com>
2026-01-21 21:59:32 +0530

    Learn how to make router

--- 07_reactRouter/src/App.jsx
+++ 07_reactRouter/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import Home from './Components/Home/Home'
+import Footer from './Components/Footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <Header/>
     </>
   )
 }
-
 export default App
--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
